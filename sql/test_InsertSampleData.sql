b622d6ceb33d6a1a5d5e7a75b711903b43e2c95d
yuka matsumura <mtmryuk01050607@docomo.ne.jp>
2017-09-05 12:51:34 +0900

    sql文修正です

--- sql/test_InsertSampleData.sql
+++ sql/test_InsertSampleData.sql
@@ -2,18 +2,20 @@
 
 use test_choitabi;
 
+delete from tour;
 insert into tour values
 ('サンプルツアー1', null, 15000, 30, 20170901, 'A駅'),
 ('サンプルツアー2', null, 15000, 30, 20170901, 'B駅'),
 ('サンプルツアー3', null, 15000, 30, 20170901, 'C駅');
 
+delete from categories_area;
 insert into categories_area values
 (null, '東北'),
 (null, '関東'),
 (null, '関西'),
 (null, '九州');
 
-
+delete from categories_theme;
 insert into categories_theme values
 (null, 'グルメ'),
 (null, '名所'),
