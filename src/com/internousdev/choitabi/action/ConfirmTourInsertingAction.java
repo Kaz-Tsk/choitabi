aea368fda1b49b4767f264e6d0c3603aa254a9e4
yuka matsumura <mtmryuk01050607@docomo.ne.jp>
2017-09-08 16:29:45 +0900

    xml編集　＆　新規ツアーの入力情報を確認するクラスを追加しました（中身は未作成です）

---
+++ src/com/internousdev/choitabi/action/ConfirmTourInsertingAction.java
@@ -0,0 +1,21 @@
+package com.internousdev.choitabi.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ConfirmTourInsertingAction extends ActionSupport{
+
+
+
+
+
+
+
+
+	public String execute(){
+
+		String result = SUCCESS;
+
+		return result;
+
+	}
+}
