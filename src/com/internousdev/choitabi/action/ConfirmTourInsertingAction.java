c9d2a855277685bfd12f36aa90ec7efa5a1fa0e3
yuka matsumura <mtmryuk01050607@docomo.ne.jp>
2017-09-11 10:08:16 +0900

    アクション・DAOクラス復旧しました。これからつなげていきます。

--- src/com/internousdev/choitabi/action/ConfirmTourInsertingAction.java
+++ src/com/internousdev/choitabi/action/ConfirmTourInsertingAction.java
@@ -1,21 +1,36 @@
 package com.internousdev.choitabi.action;
 
-import com.opensymphony.xwork2.ActionSupport;
+public class ConfirmTourInsertingAction {
 
-public class ConfirmTourInsertingAction extends ActionSupport{
+	/**@author YUKA MATSUMURA
+	 * @since 2017/09/08
+	 * @version 1.1
+	 *
+	 * 管理画面から、ツアーの新規登録時に使用するアクションです。
+	 * フォームから入力されたデータに漏れや不適切な部分（価格に文字列が入っている場合など）がないかをチェックし、
+	 * OKならばSUCCESSを返します。
+	 *（※11朝現在、未完成です）
+	 * */
+
+	/*入力されたツアー名*/
+	private String newTourName;
+
+	/*入力された価格*/
+	private String newTourPrice;
+
+	/*入力された定員*/
+	private String newTourPersons;
+
+	/*入力された出発地*/
+	private String newTourDeparture;
+
+	/**/
 
 
 
 
 
 
+	/*以下、変数のgetter/setter-----------------------------------------------------*/
 
-
-	public String execute(){
-
-		String result = SUCCESS;
-
-		return result;
-
-	}
 }
