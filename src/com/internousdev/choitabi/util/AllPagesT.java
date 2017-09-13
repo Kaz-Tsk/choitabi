d681b563626da9b19a344d6f39bcb58187b22719
Shun Nagao <ngshn95@gmail.com>
2017-09-13 11:47:29 +0900

    AllPagesT、PageObjectTourを作成しました。

--- src/com/internousdev/choitabi/util/AllPagesT.java
+++ src/com/internousdev/choitabi/util/AllPagesT.java
@@ -1,12 +1,82 @@
-/**
- * 
- */
 package com.internousdev.choitabi.util;
 
+import java.util.ArrayList;
+
+import com.internousdev.choitabi.dto.TourDTO;
+
 /**
- * @author internousdev
- *
+ * 渡されたリストに対してページネーションを行うクラス
+ * @author SHUN NAGAO
+ * @since 2017/09/13
+ * @version 1.0
  */
 public class AllPagesT {
+
+	/**
+	 * リストをページネーションして本の形に変換し、jspで表示するために格納する
+	 */
+
+
+	/**
+	 * 渡されたリストを1ページにcount個までオブジェクトが掲載された本の形に変換して返す
+	 * @param list DTOが格納されたArrayList
+	 * @param count 1ページあたりに掲載されるオブジェクトの個数
+	 * @return displayBook
+	 */
+	public ArrayList<PageObjectTour> paginate(ArrayList<TourDTO> list, int count) {
+
+		ArrayList<PageObjectTour> displayBook =new ArrayList<PageObjectTour>();
+		int itemCount = count;
+		int index = 0;
+		int maxPages = 0;
+
+		if((list.size() % count) == 0) {
+			maxPages = list.size() / count;
+		} else {
+			maxPages = (list.size() / count) + 1;
+		}
+
+
+
+		for(int i = 0; i < maxPages; i++) {
+			PageObjectTour page = new PageObjectTour();
+
+			for(int j = index; j < itemCount; j++) {
+				if(list.size() == j) {
+					break;
+				}
+				page.addPaginatedList(list,j);
+			}
+
+
+			page.setPageId(i);
+			displayBook.add(page);
+
+			index += count;
+			itemCount += count;
+		}
+		return displayBook;
+	}
+
+	/**
+	 * 渡されたリスト数と1ページあたりに掲載するオブジェクト数から総ページ数を返す
+	 * @param list DTOが格納されたArrayList
+	 * @param count 1ページあたりに掲載されるオブジェクトの個数
+	 * @return maxPages
+	 */
+	public int getMaxPage(ArrayList<?> list, int count) {
+
+		int maxPages = 0;
+
+		if((list.size() % count) == 0) {
+			maxPages = list.size() / count;
+		} else {
+			maxPages = (list.size() / count) + 1;
+		}
+
+
+		return maxPages;
+	}
+
 
 }
--- src/com/internousdev/choitabi/util/PageObjectTour.java
+++ src/com/internousdev/choitabi/util/PageObjectTour.java
@@ -1,12 +1,72 @@
-/**
- * 
- */
 package com.internousdev.choitabi.util;
+import java.util.ArrayList;
+
+import com.internousdev.choitabi.dto.TourDTO;
+
+
 
 /**
- * @author internousdev
- *
+ * ページネーションが必要なリストのために、1ページごとの情報を保持するためのクラス
+ * @author SHUN NAGAO
+ * @since 2017/09/13
+ * @version 1.0
  */
 public class PageObjectTour {
 
+	/**
+	 * ページを識別するID
+	 */
+	private int pageId;
+
+	/**
+	 * 当該1ページに掲載される商品のリスト
+	 */
+	private ArrayList<TourDTO> paginatedItemList = new ArrayList<TourDTO>();
+
+
+
+
+	/**
+	 * ページネートされた商品リストを取得するメソッド
+	 * @return  paginatedItemList
+	 */
+	public ArrayList<TourDTO> getPaginatedList() {
+			return paginatedItemList;
+			}
+
+	/**
+	 * ページネートされた商品リストを格納するメソッド
+	 * @param list ページネーションが完了した1ページごとのリスト
+	 */
+	public void setPaginatedList(ArrayList<TourDTO> list) {
+			paginatedItemList = list;
+	}
+
+	/**
+	 * AllPagesクラスのpaginate()において使われる
+	 * これによってDTOのインスタンスが当該ページにひとつずつ格納されていく
+	 * @param list これからページネーション処理を行うリスト
+	 * @param j 繰り返し用変数
+	 */
+	public void addPaginatedList(ArrayList<TourDTO> list, int j) {
+			paginatedItemList.add((TourDTO)list.get(j));
+			}
+
+
+
+	/**
+	 * ページID取得メソッド
+	 * @return pageId
+	 */
+	public int getPageId() {
+		return pageId;
+	}
+
+	/**
+	 * ページID格納メソッド
+	 * @param pageId セットする pageId
+	 */
+	public void setPageId(int pageId) {
+		this.pageId = pageId;
+	}
 }
