de8e775d86d03d2107a7e3e8e6101f1367c2cfbb
kanako miyazono <tune.dec621@gmail.com>
2017-09-14 13:05:45 +0900

    お問い合わせ関連：SESSION機能を追加し始めました。

--- src/com/internousdev/choitabi/action/FirstContactAction.java
+++ src/com/internousdev/choitabi/action/FirstContactAction.java
@@ -1,27 +1,56 @@
 package com.internousdev.choitabi.action;
 
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
 import com.opensymphony.xwork2.ActionSupport;
 
 /**
- * 問い合わせ画面へ遷移するアクションクラス
- *
+ * お問い合わせ画面へ遷移するアクション
  * @author kanako miyazono
- * @version 1.0
- * @since 1.0
+ * @since 2017/9/14
+ * @version 1.1
  */
-public class FirstContactAction extends ActionSupport {
+public class FirstContactAction extends ActionSupport implements SessionAware {
 
-    /**
-     * 生成されたシリアルナンバー
-     */
-    private static final long serialVersionUID = 6239062489411744537L;
+	/**
+	 * シリアルバージョンID
+	 */
+	private static final long serialVersionUID = -8491587999319615832L;
+	/**
+	 * セッション
+	 */
+	private Map<String, Object> session;
 
-    /**
-     * 問い合わせページへ遷移する為のメソッド
-     *
-     * @return 常にSUCCESSを返します。
-     */
-    public String execute() {
-        return SUCCESS;
-    }
+	/**
+	 * お問い合わせ画面へ遷移するためのメソッド
+	 * @author kanako miyazono
+	 * @since 2017/8/22
+	 * @version 1.1
+	 * @return SUCCESS
+	 */
+	public String execute() {
+		String rtn = SUCCESS;
+		;
+
+		return rtn;
+	}
+
+	/**
+	 * セッションを取得するメソッド
+	 * @return session セッション
+	 */
+	public Map<String, Object> getSession() {
+		return session;
+	}
+
+	/**
+	 * セッションを格納する
+	 * @param session  セッション
+	 */
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
+
 }
