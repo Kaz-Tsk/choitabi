9cd1630f2b4d32eab449a83b3c52edfc007a54f9
kanakomiyazono <tune.dec621@gmail.com>
2017-09-21 10:25:17 +0900

    単体試験準備（testDTO）始めました。

---
+++ src/com/internousdev/choitabi/dto/test/ContactConpleteDTOTest.java
@@ -0,0 +1,39 @@
+package com.internousdev.choitabi.dto.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ContactConpleteDTOTest {
+
+	@Test
+	public void testGetContact_name() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testSetForm_name_a() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetContact_mailAddress() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testSetContact_mailAddress() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetContact_contents() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testSetContact_contents() {
+		fail("まだ実装されていません");
+	}
+
+}
---
+++ src/com/internousdev/choitabi/dto/test/KanriContactDTOTest.java
@@ -0,0 +1,49 @@
+package com.internousdev.choitabi.dto.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class KanriContactDTOTest {
+
+	@Test
+	public void testGetContact_name() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testSetContact_name() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetContact_mailAddress() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testSetContact_mailAddress() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetContact_contents() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testSetContact_contents() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetRegistrationDate() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testSetRegistrationDate() {
+		fail("まだ実装されていません");
+	}
+
+}
