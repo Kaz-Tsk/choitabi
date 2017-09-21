84ac5f9a3aafe106d1831531b5fea5f2201cade4
yuka matsumura <mtmryuk01050607@docomo.ne.jp>
2017-09-21 11:07:34 +0900

    SelectTourDTOTest更新中です。旅癒さんのテストケースを参考にメモ書きを追加しています。

--- src/com/internousdev/choitabi/dto/test/SelectTourDTOTest.java
+++ src/com/internousdev/choitabi/dto/test/SelectTourDTOTest.java
@@ -4,12 +4,52 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.internousdev.choitabi.dto.SelectTourDTO;
+
 public class SelectTourDTOTest {
 
+	/*個人的メモ
+	 * 旅癒さん参考
+	 * 変数は「expected」「actual」の2つを用意。答えの予想値として設定した値と、実際の戻り値を表す。
+	 * 「expected」をsetterでセットし、「actual」をgetterで取得する。
+	 * 両者が同じであれば試験は成功と判断される。
+	 * 判断するためのメソッドが「assertEquals（expected, actual）」引数として渡した値が同値であればOKと判断するもの
+	 * private int tourId;
+	 * private String tourName;
+	 * private int price;
+	 * private int persons;
+	 * private String departure;
+	 * private String region;
+	 * private String prefectures;
+	 * private String theme;
+	 * private String comment;
+	 * private String img;
+	 */
+
 	@Test
-	public void testGetTourId() {
+	public void testGetTourId1() {
+		SelectTourDTO dto = new SelectTourDTO();
+		int expected = 0;
+
+		dto.setTourId(expected);
+		int actual = dto.getTourId();
+
+		assertEquals(expected, actual);
+	}
+
+	public void testGetTourId2() {
 		fail("まだ実装されていません");
 	}
+	public void testGetTourId3() {
+		fail("まだ実装されていません");
+	}
+	public void testGetTourId4() {
+		fail("まだ実装されていません");
+	}
+
+
+	//------------------------------------------------
+
 
 	@Test
 	public void testSetTourId() {
