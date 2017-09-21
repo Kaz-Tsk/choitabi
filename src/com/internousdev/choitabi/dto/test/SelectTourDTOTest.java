87764aefe8e4abf5d64f511b589a11887f09fd82
yuka matsumura <mtmryuk01050607@docomo.ne.jp>
2017-09-21 10:46:05 +0900

    SelectDTO(管理者側でツアー管理に使用するDTO)のテストケースを作成しました。

---
+++ src/com/internousdev/choitabi/dto/test/SelectTourDTOTest.java
@@ -0,0 +1,109 @@
+package com.internousdev.choitabi.dto.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SelectTourDTOTest {
+
+	@Test
+	public void testGetTourId() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testSetTourId() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetTourName() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testSetTourName() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetPrice() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testSetPrice() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetPersons() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testSetPersons() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetDeparture() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testSetDeparture() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetRegion() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testSetRegion() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetPrefectures() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testSetPrefectures() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetTheme() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testSetTheme() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetComment() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testSetComment() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetImg() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testSetImg() {
+		fail("まだ実装されていません");
+	}
+
+}
