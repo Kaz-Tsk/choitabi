0b2d5923c7d751be49199e5178f64b731fcd6901
Shun Nagao <ngshn95@gmail.com>
2017-09-22 13:18:52 +0900

    不要なクラス等を削除しました。

--- src/com/internousdev/choitabi/action/GoPaymentAction.java
+++ src/com/internousdev/choitabi/action/GoPaymentAction.java
@@ -1,245 +1,227 @@
 package com.internousdev.choitabi.action;
 
-import java.sql.SQLException;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
 
-import com.internousdev.choitabi.dao.InsertCartDAO;
+import com.internousdev.choitabi.dao.MypageDAO;
+import com.internousdev.choitabi.dao.SelectCartDAO;
 import com.internousdev.choitabi.dto.CartDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
 /**
- * カート画面から購入画面へ遷移するクラス
+ * カート画面から支払い画面に遷移するクラス
  * @author SHUN NAGAO
- *　@since 2017/09/08
+ * @since 2017/09/22
  * @version 1.1
  */
-
-public class GoPaymentAction  extends ActionSupport implements SessionAware{
+public class GoPaymentAction extends ActionSupport implements SessionAware {
+	/**
+	 * シリアル
+	 */
+	private static final long serialVersionUID = -4199669519568280545L;
+	/**
+	 * セッション
+	 */
+	private Map<String, Object> session;
+	/**
+	 * ユーザーID
+	 */
+	private int user_id;
+	/**
+	 * 合計金額
+	 */
+	private BigDecimal order_count = BigDecimal.ZERO;
+	/**
+	 * 小計
+	 */
+	private BigDecimal subtotal = BigDecimal.ZERO;
+	/**
+	 * カート内の商品情報を入れるリスト
+	 */
+	private ArrayList<CartDTO> cartList = new ArrayList<>();
+	/**
+	 * ユーザー情報を入れるリスト
+	 */
+	private ArrayList<CartDTO> userList = new ArrayList<>();
+	/**
+	 * 所持ポイント
+	 */
+	private int point;
+	/**
+	 * 商品合計金額
+	 */
+	private BigDecimal total_price = BigDecimal.ZERO;
 
 	/**
-     * シリアルID
-     */
-    private static final long serialVersionUID = 8015241731370150943L;
+	 * カート・ユーザー・商品情報を格納する実行メソッド
+	 * @author SHUN NAGAO
+	 * @since 2017/09/22
+	 * @version 1.1
+	 */
 
-    /**
-     * ユーザーID
-     */
-    private int user_id;
+	public String execute() {
+		String result = ERROR;
 
-    /**
-     * ツアーID
-     */
-    private int tour_id;
+		if (session.containsKey("userId")) {
+			user_id = (int) session.get("userId");
 
-    /**
-     * ツアー名
-     */
-    private String tour_name;
+			MypageDAO myDao = new MypageDAO();
+			SelectCartDAO cartDao = new SelectCartDAO();
 
-    /**
-     * 単価
-     */
-    private int price;
+			userList = myDao.select(user_id);
+			cartList = cartDao.selectedItem(user_id);
+			setPoint(userList.get(0).getPoint());
 
-    /**
-     * 予約人数
-     */
-    private int order_count;
+			for(int i=0;i<cartList.size();i++){
+				total = total.add(cartList.get(i).getSubtotal());
+			}
 
-    /**
-     * 画像パスファイル
-     */
-    private String img;
+			if (cartList.size() > 0) {
+				return SUCCESS;
+			}
+		} else {
+			return LOGIN;
+		}
+		return result;
+	}
 
-    /**
-     * 合計金額
-     */
-    private int total_price;
+	/**
+	 * セッション取得メソッド
+	 * @return session セッション
+	 */
+	public Map<String, Object> getSession() {
+		return session;
+	}
 
-    /**
-     * カート内の商品情報を入れるリスト
-     */
-    private ArrayList<CartDTO> cartList=new ArrayList<>();
+	/**
+	 * セッション格納メソッド
+	 * @param session セッション
+	 */
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
 
-    /**
-     *セッション情報
-     */
-    private Map<String,Object> session;
+	/**
+	 * 合計金額を取得するメソッド
+	 * @return amountAll 合計金額
+	 */
+	public BigDecimal getAmountAll() {
+		return amountAll;
+	}
 
-    /**
-     * 実行メソッド
-     * 処理内容と順番
-     * 1：セッション情報を持っているか判断
-     * 2：session内のuserIdを使用し、カートへ登録された情報を取得
-     * 3：カート内の情報を元に合計金額を算出
-     */
-    public String execute() throws SQLException{
-       // String result=ERROR;
-    	String result="success";
-        if (session.containsKey("user_id")) {
-            user_id = (int)session.get("user_id");
+	/**
+	 * 合計金額を格納するメソッド
+	 * @param amountAll 合計金額
+	 */
+	public void setAmountAll(BigDecimal amountAll) {
+		this.amountAll = amountAll;
+	}
 
-            InsertCartDAO dao = new InsertCartDAO();
-            cartList = dao.selected(user_id);
+	/**
+	 * 小計を取得するメソッド
+	 * @return subtotal 小計
+	 */
+	public BigDecimal getSubtotal() {
+		return subtotal;
+	}
 
-            for(int i = 0; i < cartList.size(); i++ ){
-            	total_price += (cartList.get(i).getPrice())*(cartList.get(i).getOrder_count());
-                }
+	/**
+	 * 小計を格納するメソッド
+	 * @param subtotal 小計
+	 */
+	public void setSubtotal(BigDecimal subtotal) {
+		this.subtotal = subtotal;
+	}
 
-            result = SUCCESS;
-        }
+	/**
+	 * カートリストを取得するメソッド
+	 * @return cartList カートリスト
+	 */
+	public ArrayList<CartDTO> getCartList() {
+		return cartList;
+	}
 
+	/**
+	 * カートリストを格納するメソッド
+	 * @param cartList カートリスト
+	 */
+	public void setCartList(ArrayList<CartDTO> cartList) {
+		this.cartList = cartList;
+	}
 
+	/**
+	 * ユーザーリストを取得するメソッド
+	 * @return userList ユーザーリスト
+	 */
+	public ArrayList<UserDTO> getUserList() {
+		return userList;
+	}
 
-        return result;
-        }
+	/**
+	 * ユーザーリストを格納するメソッド
+	 * @param userList ユーザーリスト
+	 */
+	public void setUserList(ArrayList<UserDTO> userList) {
+		this.userList = userList;
+	}
 
+	/**
+	 * ユーザーIDを取得するメソッド
+	 * @return user_id ユーザーID
+	 */
+	public int getUser_id() {
+		return user_id;
+	}
 
-    /**
-     * ユーザーIDを取得するメソッド
-     * @return user_id ユーザーID
-     */
-    public int getUser_id() {
-        return user_id;
-        }
+	/**
+	 * ユーザーIDを格納するメソッド
+	 * @param user_id ユーザーID
+	 */
+	public void setUserId(int user_id) {
+		this.user_id = user_id;
+	}
 
-    /**
-     * ユーザーIDを格納するメソッド
-     * @param user_id セットする ユーザーID
-     */
-    public void setUser_id(int user_id) {
-        this.user_id = user_id;
-        }
+	/**
+	 * ポイントを取得するメソッド
+	 * @return point ポイント
+	 */
+	public int getPoint() {
+		return point;
+	}
 
-    /**
-     * ツアーIDを取得するメソッド
-     * @return tour_id ツアーID
-     */
-    public int gettour_id() {
-        return tour_id;
-        }
+	/**
+	 *  ポイントを格納するメソッド
+	 * @param point ポイント
+	 */
+	public void setPoint(int point) {
+		this.point = point;
+	}
 
-    /**
-     * ツアーIDを格納するメソッド
-     * @param tour_id セットする ツアーID
-     */
-    public void settour_id(int tour_id) {
-        this.tour_id = tour_id;
-        }
+	/**
+	 *  ユーザーIDを格納するメソッド
+	 * @param user_id　ユーザーID
+	 */
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
 
-    /**
-     * ツアー名を取得するメソッド
-     * @return tour_name ツアー名
-     */
-    public String gettour_name() {
-        return tour_name;
-        }
+	/**
+	 * 商品合計金額を取得するメソッド
+	 * @return total 商品合計金額
+	 */
+	public BigDecimal getTotal() {
+		return total;
+	}
 
-    /**
-     * ツアー名を格納するメソッド
-     * @param tour_name セットする ツアー名
-     */
-    public void settour_name(String tour_name) {
-        this.tour_name = tour_name;
-        }
-
-    /**
-     * 単価を格取得するメソッド
-     * @return price 単価
-     */
-    public int getPrice() {
-        return price;
-        }
-
-    /**
-     * 単価を格納するメソッド
-     * @param price セットする 単価
-     */
-    public void setPrice(int price) {
-        this.price = price;
-        }
-
-    /**
-     * 合計金額を取得するメソッド
-     * @return total_price 合計金額
-     */
-    public int gettotal_price() {
-        return total_price;
-        }
-
-    /**
-     * 合計金額を格納するメソッド
-     * @param total_price セットする 合計金額
-     */
-    public void setAmount(int total_price) {
-        this.total_price = total_price;
-        }
-
-    /**
-     * カート内のツアー情報を入れるリストを取得するメソッド
-     * @return cartList カート内のツアー情報を入れるリスト
-     */
-    public ArrayList<CartDTO> getCartList() {
-        return cartList;
-        }
-
-    /**
-     * カート内のツアー情報を入れるリストを格納するメソッド
-     * @param cartList セットする カート内のツアー情報を入れるリスト
-     */
-    public void setCartList(ArrayList<CartDTO> cartList) {
-        this.cartList = cartList;
-        }
-
-    /**
-     * セッション情報を取得するメソッド
-     * @return session セッション情報
-     */
-    public Map<String, Object> getSession() {
-        return session;
-        }
-
-    /**
-     * セッションを格納するメソッド
-     * @param session セットする セッション情報
-     */
-    public void setSession(Map<String, Object> session) {
-        this.session = session;
-        }
-
-    /**
-     * 注文数を取得するメソッド
-     * @return order_count 注文数
-     */
-    public int getorder_count() {
-        return order_count;
-        }
-
-    /**
-     * 注文数を格納するメソッド
-     * @param order_count セットする 注文数
-     */
-    public void setorder_count(int order_count) {
-        this.order_count = order_count;
-        }
-
-    /**
-     * 画像パスファイルを取得するメソッド
-     * @return img 画像パス
-     */
-    public String getImg() {
-        return img;
-        }
-
-    /**
-     * 画像パスファイルを格納するメソッド
-     * @param img セットする 画像パス
-     */
-    public void setImg(String img) {
-        this.img = img;
-        }
+	/**
+	 *  商品合計金額を格納するメソッド
+	 * @param total 商品合計金額
+	 */
+	public void setTotal(BigDecimal total) {
+		this.total = total;
+	}
 
 }
